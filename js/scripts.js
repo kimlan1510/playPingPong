8e8f94c08a1cd531a10dd305faef7ca95cd5edde
Kimlan Nguyen <kimlan1510@yahoo.com>
2017-05-05 09:13:06 -0700

    Add if statements to output ping or pong or ping-pong or numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
 function playPingPong (number){
   if (!isNaN(number) === false || number === ""){
-    alert("Please enter a number.");
+    alert("Please enter an integer greater than 0.");
   }
   else {
     number = parseFloat(number);
     if(number <= 0){
-      alert("Please enter a number great than 0.")
+      alert("Please enter an integer greater than 0.")
     }
     else if(Number.isInteger(number) === false){
       alert("Please enter an integer, 1, 2, 7 ,etc.");
     }
+    else{
+      var pingOrpong = [];
+      for(i=1, i=< number, i++){
+        if(number%3 === 0 && number%5 === 0){
+          pingOrpong.push("ping-pong");
+        }
+        else if(number%3 === 0){
+          pingOrpong.push("ping");
+        }
+        else if(number%5 === 0){
+          pingOrpong.push("pong");
+        }
+        else{
+          pingOrpong.push("i");
+        }
+      }
+    }
   }
+  return pingOrpong;
 }
 
 
 $(document).ready(function(){
   $("form#pingpong").submit(function(){
     var numberToPlay = $("input#user_input").val();
-    playPingPong(numberToPlay);
+    var result = playPingPong(numberToPlay);
+    $("ul.list").
   });
 });

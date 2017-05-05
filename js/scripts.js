bca0fc8cb48206ce7a39b25e88fb8c7c3c4aa503
Kimlan Nguyen <kimlan1510@yahoo.com>
2017-05-05 08:55:20 -0700

    Add if statement to alert users about errors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
+function playPingPong (number){
+  if (!isNaN(number) === false || number === ""){
+    alert("Please enter a number.");
+  }
+  else {
+    number = parseFloat(number);
+    if(number <= 0){
+      alert("Please enter a number great than 0.")
+    }
+    else if(Number.isInteger(number) === false){
+      alert("Please enter an integer, 1, 2, 7 ,etc.");
+    }
+  }
+}
 
 
-
-(document).ready(function(){
-  var numberToPlay = $("form#pingpong").("input#user_input").val();
+$(document).ready(function(){
+  $("form#pingpong").submit(function(){
+    var numberToPlay = $("input#user_input").val();
+    playPingPong(numberToPlay);
+  });
 });
